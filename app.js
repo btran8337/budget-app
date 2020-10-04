8bbf5a8e207b03d18f2f1c2f8c5ca7f9e5351168
Brandon <btran8337@yahoo.com>
2020-10-04 16:07:04 -0500

    created income and expense function constructors

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
-  // Some code
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
 })();
 
 // UI CONTROLLER
@@ -44,7 +65,6 @@ var controller = (function (budgetCtrl, UICtrl) {
   var ctrlAddItem = function () {
     // 1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
 
     // 2. Add item to the budget controller
     // 3. Add the item to the UI
